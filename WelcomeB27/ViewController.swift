7e0d1c52fe6bdfe4a2a048445d90ce7205e30702
omar9369 <omar.h02@outlook.com>
2021-03-17 16:15:08 -0500

    Button
    Change color of background each time the button is pressed

--- WelcomeB27/ViewController.swift
+++ WelcomeB27/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var i = 0;
+    var check = false;
+    @IBOutlet weak var counter: UILabel!
+    
+    
+    @IBAction func bttn(_ sender: Any) {
+        
+        self.view.backgroundColor = UIColor.randomColor();
+        if(i == 15){
+            check = true;
+        }
+        else if(i == 0){
+            check = false;
+        }
+        if(check == true){
+            i = i - 1;
+        }else{
+            i = i + 1;
+        }
+        counter.text = "\(i)";
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+    }  
 }
 
+//Inheriting from UIColor
+extension UIColor {
+    static func randomColor(saturation: CGFloat = 1, brightness: CGFloat = 1, alpha: CGFloat = 1) -> UIColor {
+        let hue = CGFloat(arc4random_uniform(361)) / 360.0
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
+}
